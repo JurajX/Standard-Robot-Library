43f0b6680ed75ef7e217b9f00190b1793d74a9e4
Juraj <37395233+JurajX@users.noreply.github.com>
2022-06-05 20:13:43 +0200

    capitalise tidy

--- make/checks.mk
+++ make/checks.mk
@@ -1,7 +1,7 @@
 .Phony: tidy format
 
 tidy: export COMPILER=clang
-tidy: export BUILD_TYPE=tidy
+tidy: export BUILD_TYPE=Tidy
 tidy:
 	${MAKE} build/cmake
 	@echo this echo is necessary else the path is not set-up correctly &&\
