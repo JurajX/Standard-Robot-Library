d4058a691f4eb08274862ef09265652085ac7aa4
Juraj <37395233+JurajX@users.noreply.github.com>
2023-05-29 15:25:12 +0200

    clean compile options

--- cmake/add_compile_options.cmake
+++ cmake/add_compile_options.cmake
@@ -5,9 +5,9 @@ function(add_compile_options)
     cmake_parse_arguments(USER "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CLANG true)
+        set(CLANG TRUE)
     elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        set(GCC true)
+        set(GCC TRUE)
     else()
         message(FATAL_ERROR "Supported compilers are Clang and GCC. Got ${CMAKE_CXX_COMPILER_ID}.")
     endif()
@@ -28,86 +28,78 @@ function(add_compile_options)
         -Wdouble-promotion                      # a float is implicitely promoted to double
         -Wfloat-equal                           # floating-point values used in equality comparisons
         -Wnon-virtual-dtor                      # a class with virtual functions has a non-virtual destructor
-        -Wnull-dereference                      # a null dereference is detected
         -Wold-style-cast                        # c-style casts
         -Woverloaded-virtual                    # overloaded (not overrided) virtual function
         -Wredundant-decls                       # a variable declared more than once in the same scope
         -Wno-overlength-strings                 # a string constants that are longer than the length specified in the C standard
         -Wsuggest-override                      # overriding virtual functions that are not marked override
         -Wextra-semi                            # redundant semicolons after in-class function definitions
+
+        $<$<BOOL:${CLANG}>:-Wmost>              # most warnings
+        $<$<BOOL:${CLANG}>:-Wnull-dereference>  # a null dereference is detected
+
+        $<$<BOOL:${GCC}>:-Wuseless-cast>        # unnecessary cast
+        $<$<BOOL:${GCC}>:-Wsign-conversion>     # implicit sign conversions
+        $<$<BOOL:${GCC}>:-Warith-conversion>    # implicit arithmetic conversions
     )
-    if(CLANG)
-        list(APPEND WARNINGS
-            -Wmost                              # most warnings
-        )
-    elseif(GCC)
-        list(APPEND WARNINGS
-            -Wuseless-cast                      # unnecessary cast
-            -Wsign-conversion                   # implicit sign conversions
-            -Warith-conversion                  # implicit arithmetic conversions
-        )
-    endif()
 
 
 # =========================== Default Options
     list(APPEND DEFAULT_OPTIONS
-        -fexceptions                            # throw from c++ through c back to c++ survives
-        # -fcf-protection                         # control flow integrity protection
-        -fvisibility-inlines-hidden             # forbids to compare pointers to inline functions
-        -fvisibility=default                    # symbols in libraries to be explicitly exported to avoid conflicts
-        # -march=native                           # optimise for local architecture
-        -pipe                                   # avoid writing temporary files
+        -fexceptions                                                # throw from c++ through c back to c++ survives
+        -fvisibility-inlines-hidden                                 # forbids to compare pointers to inline functions
+        -fvisibility=default                                        # symbols in libraries to be explicitly exported to avoid conflicts
+        -pipe                                                       # avoid writing temporary files
+        -fcf-protection                                             # control flow integrity protection
+        $<$<BOOL:${GCC}>:-fstack-clash-protection>                  # increased reliability of stack overflow detection
+        $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fstack-protector-strong>    # more performant stack protector   not for emscripten
+        $<$<NOT:$<CONFIG:Debug>>:-D_FORTIFY_SOURCE=2>               # run-time buffer overflow detection (needs at least -O1)
+        $<$<BOOL:${EMSCRIPTEN}>:-msimd128>                          # turn on SIMD in emscripten
+        # -march=native                                               # optimise for local architecture
     )
-    if(GCC)
-        list(APPEND DEFAULT_OPTIONS
-            -fstack-clash-protection            # increased reliability of stack overflow detection
-        )
-    endif()
-    list(APPEND DEFAULT_OPTIONS $<$<NOT:$<CONFIG:Debug>>:-D_FORTIFY_SOURCE=2>)              # run-time buffer overflow detection (needs at least -O1)
-    list(APPEND DEFAULT_OPTIONS $<$<NOT:$<BOOL:${EMSCRIPTEN}>>:-fstack-protector-strong>)   # more performant stack protector   not for emscripten
-    list(APPEND DEFAULT_OPTIONS $<$<BOOL:${EMSCRIPTEN}>:-msimd128>)                         # turn on SIMD in emscripten
+
+
+
+
 
 
 # =========================== Coverage Options
     list(APPEND COVERAGE_OPTIONS
-        -fno-inline                             # no inline
-        -g                                      # debugging information in the operating system's native format
+        -fno-inline                                     # no inline
+        -g                                              # debugging information in the operating system's native format
+        $<$<BOOL:${CLANG}>:-fprofile-instr-generate>    # generate instrumented code to collect execution counts
+        $<$<BOOL:${CLANG}>:-fcoverage-mapping>          # generate coverage mapping to enable code coverage analysis
+        $<$<BOOL:${GCC}>:-ftest-coverage>               # produce a notes file that can use to show program coverage
+        $<$<BOOL:${GCC}>:-fprofile-arcs>                # records how many times each branch/call is executed
     )
-    if(CLANG)
-        list(APPEND COVERAGE_OPTIONS
-            -fprofile-instr-generate            # generate instrumented code to collect execution counts
-            -fcoverage-mapping                  # generate coverage mapping to enable code coverage analysis
-        )
-    elseif(GCC)
-        list(APPEND COVERAGE_OPTIONS
-            -ftest-coverage                     # produce a notes file that can use to show program coverage
-            -fprofile-arcs                      # records how many times each branch/call is executed
-        )
-    endif(CLANG)
 
 
 # =========================== Debug Options
+    set(GCC_FIXED TRUE)
+    if (APPLE AND GCC)
+        set(GCC_FIXED FALSE)
+    endif()
+
     list(APPEND DEBUG_OPTIONS
         # -fsanitize=thread                                   # thread sanitiser      cannot be used in combination with address and leak sanitisers
         # -fsanitize=memory                                   # memory sanitiser      not supported for mac
         # -fsanitize-memory-track-origins                     # track memory origin
 
-        -fsanitize=address                                  # address sanitiser
-        -fsanitize=pointer-subtract                         # instrument subtraction with pointer operands; needs address sanitiser
-        -fsanitize=pointer-compare                          # instrument comparison operation; needs address sanitiser
-        -fsanitize=leak                                     # leak sanitiser
-        -fsanitize-address-use-after-return=always          # detect stack use after return problems
-        -fsanitize-address-use-after-scope                  # sanitization of local variables
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=address>                  # address sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=pointer-subtract>         # instrument subtraction with pointer operands; needs address sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=pointer-compare>          # instrument comparison operation; needs address sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=leak>                     # leak sanitiser; needs address sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize-address-use-after-scope>  # sanitization of local variables; needs address sanitiser
 
-        -fsanitize=undefined                                # undefined sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=undefined>                # undefined sanitiser
+        $<$<BOOL:${GCC_FIXED}>:-fsanitize=float-divide-by-zero>     # floating point division by zero
 
-        -fsanitize=integer                                  # checks for undefined or suspicious integer behavior
-        -fsanitize=implicit-integer-arithmetic-value-change # catches implicit conversions that change the arithmetic value of the integer
-
-        -fsanitize=float-divide-by-zero                     # floating point division by zero
-        -fsanitize=implicit-conversion                      # checks for suspicious behavior of implicit conversions
-        -fsanitize=local-bounds                             # out of bounds array indexing
-        -fsanitize=nullability                              # passing, assigning, and returning null
+        $<$<BOOL:${CLANG}>:-fsanitize-address-use-after-return=always>          # detect stack use after return problems; needs address sanitiser
+        $<$<BOOL:${CLANG}>:-fsanitize=integer>                                  # checks for undefined or suspicious integer behavior
+        $<$<BOOL:${CLANG}>:-fsanitize=implicit-integer-arithmetic-value-change> # implicit conversions that change the value of the integer
+        $<$<BOOL:${CLANG}>:-fsanitize=implicit-conversion>                      # checks for suspicious behavior of implicit conversions
+        $<$<BOOL:${CLANG}>:-fsanitize=local-bounds>                             # out of bounds array indexing
+        $<$<BOOL:${CLANG}>:-fsanitize=nullability>                              # passing, assigning, and returning null
 
         -fasynchronous-unwind-tables                        # generate unwind table that can be used for stack unwinding from asynchronous events
         -fno-omit-frame-pointer                             # for nicer stack traces in error messages
